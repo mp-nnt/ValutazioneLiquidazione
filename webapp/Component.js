285ee2c8c69f50da176f6fb97aee1e19793fc525
S0020034726 <michele.pigozzo@nttdata.com>
2018-12-04 10:09:14 +0000

    Demo App

--- webapp/Component.js
+++ webapp/Component.js
@@ -25,6 +25,77 @@ sap.ui.define([
 
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
-		}
+			//set Mockdata
+			var sPath = "model/mockData/model.json";
+			this.setModel(new sap.ui.model.json.JSONModel(sPath));
+			/*
+						// get WF task data
+						var startupParameters = this.getComponentData().startupParameters;
+						var taskModel = startupParameters.taskModel;
+						var taskData = taskModel.getData();
+						var taskId = taskData.InstanceID;
+
+						// initialize WF model
+						var contextModel = new sap.ui.model.json.JSONModel("/bpmworkflowruntime/rest/v1/task-instances/" + taskId + "/context");
+						contextModel.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
+						this.setModel(contextModel);
+
+						//add actions
+						startupParameters.inboxAPI.addAction({
+							action: "Confirm",
+							label: "Conferma"
+						}, function (button) {
+							this._completeTask(taskId, true);
+						}, this);
+						startupParameters.inboxAPI.addAction({
+							action: "Save",
+							label: "Salva Bozza"
+						}, function (button) {
+							this._completeTask(taskId, false);
+						}, this);
+			*/
+		},
+		/*
+				_completeTask: function (taskId, approvalStatus) {
+					var token = this._fetchToken();
+					var oModel = this.getModel();
+					oModel.setProperty("/confirm", approvalStatus);
+
+					$.ajax({
+						url: "/bpmworkflowruntime/rest/v1/task-instances/" + taskId,
+						method: "PATCH",
+						contentType: "application/json",
+						async: false,
+						data: JSON.stringify({
+							status: "COMPLETED",
+							context: oModel.getData()
+						}),
+						headers: {
+							"X-CSRF-Token": token
+						}
+					});
+					this._refreshTask(taskId);
+				},
+
+				_fetchToken: function () {
+					var token;
+					$.ajax({
+						url: "/bpmworkflowruntime/rest/v1/xsrf-token",
+						method: "GET",
+						async: false,
+						headers: {
+							"X-CSRF-Token": "Fetch"
+						},
+						success: function (result, xhr, data) {
+							token = data.getResponseHeader("X-CSRF-Token");
+						}
+					});
+					return token;
+				},
+
+				_refreshTask: function (taskId) {
+					this.getComponentData().startupParameters.inboxAPI.updateTask("NA", taskId);
+				}
+		*/
 	});
 });
--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,9 +1,19 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"jquery.sap.global",
+	"sap/m/ObjectMarker",
+	"sap/m/MessageToast",
+	"sap/m/UploadCollectionParameter",
+	"sap/m/library",
+	"sap/ui/model/json/JSONModel",
+	"sap/ui/core/format/FileSizeFormat"
+], function (Controller, jQuery, ObjectMarker, MessageToast, UploadCollectionParameter, MobileLibrary, JSONModel, FileSizeFormat) {
 	"use strict";
 
 	return Controller.extend("com.pabz.ValutazioneLiquidazione.controller.Main", {
+
+		onInit: function () {
+		}
 
 	});
 });
